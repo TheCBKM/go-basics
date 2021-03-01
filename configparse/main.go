28fdf84f64b3c18b1197698c85df1167bc589593
Your Name <you@example.com>
2021-03-01 16:37:50 +0530

    added test coverages

--- configparse/main.go
+++ configparse/main.go
@@ -1,3 +1,10 @@
+// 1. if no config file it will panic
+// 2. if invalid file syntax it will panic
+// 3. if wrong file path it will panic
+// 4. if no field mention it will set to defaults
+// 5. if multiple config files priority be like json > yml > env
+// 6. if Unable to deqode the struct it will panic
+
 package main
 
 import (
@@ -15,13 +22,16 @@ type conf struct {
 }
 
 func setConf(name, path string) {
+	//call to set default values
 	setDefaults()
+	//set file path and name
 	viper.SetConfigName(name)
 	viper.AddConfigPath(path)
-
+	//check for file
 	if err := viper.ReadInConfig(); err != nil {
 		log.Panicf("Error reading config file, %s", err)
 	}
+	//deqode to conf struct
 	var c conf
 	err := viper.Unmarshal(&c)
 	if err != nil {
@@ -36,7 +46,6 @@ func setDefaults() {
 	viper.SetDefault("host", "default_host")
 	viper.SetDefault("password", "default_pass")
 	viper.SetDefault("dbname", "default_db")
-
 }
 func main() {
 	setConf("config", "./configs")
